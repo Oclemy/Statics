1ebebf14bac9a1492e825c39f7f7481792b9405a
Oclemy <oclemmi@gmail.com>
2023-09-08 06:13:54 +0300

    added noindex

--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,8 @@
+---
+slug: /
+sidebar_position: 1
+---
+
 # Welcome
 
 Welcome to our site.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  noIndex: true,
   title: 'Our Apps',
   tagline: 'Static Files for our apps',
   // favicon: 'img/favicon.ico',
